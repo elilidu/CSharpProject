33d4aa02a274d741fba27c28f0aca96b4d361ce3
Elias Getachew <elilidu1990@gmail.com>
2024-09-28 16:07:45 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,82 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleAppMy
+namespace CSharpDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Array of Strings
-            string[] colorArray = { "Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Black", "White", "Pink", "Brown" };
-            Console.WriteLine("Slect a umber between 0 and 9");
-            int stringSelect = Convert.ToInt32(Console.ReadLine());
-            bool validString = false;
+			Console.WriteLine("Welcome to Acme Accounting Systems \n Remember, we’re “accounting");
+			Console.WriteLine("What is your name?");// 
+			String yourName = Console.ReadLine();  // Reading the name and assign the Text value on yourname identifyer
+			Console.WriteLine("What course are you on ?");
+			String course = Console.ReadLine();    // Reading the coures  and assign the Text value on course identifyer
+			Console.WriteLine("What page Number ?");
+			String pagen = Console.ReadLine();    // Reading page number and assign the value on yourname identifyer
+			
 
-            while (!validString)
-            {
-                try
-                {
-                    Console.WriteLine("or favorite color is " + colorArray[stringSelect]);
-                    validString = true;
-                }
-                catch
-                {
-                    Console.WriteLine("sorry, that number selection is invalid. Please select a number between 0 and 9.");
-                }
-                stringSelect = Convert.ToInt32(Console.ReadLine());
-            }
+			
+			Console.ReadLine();
 
-            List<string> occupationList = new List<string>()
-            {
-                "Data Analist",
-                "UX Designer",
-                "Artist",
-                "Teacher",
-                "Lawyer",
-                "Cowboy",
-                "Athlete",
-                "Podcost Host",
-                "Softwear Developer",
-                "QA Tester"
-            };
-            Console.WriteLine("\n Select another number between 0 and 9:");
-            int listSelect = Convert.ToInt32(Console.ReadLine());
-            bool validList = false;
 
-            while (!validList)
-            {
-                try
-                {
-                    Console.WriteLine("Your new occupation is  " + occupationList[listSelect]);
-                    validList = true;
-                }
-                catch
-                {
-                    Console.WriteLine("sorry, that number selection is invalid. Please select a number between 0 and 9.");
-                }
-                 listSelect = Convert.ToInt32(Console.ReadLine());
-            }
-            int[] intArray = { 17,22,3,67,5,91,11,42,23,6};
-            Console.WriteLine("\n Select a third number between 0 and 9");
-            int intSelect=Convert.ToInt32(Console.ReadLine());
-            bool valdInt = false;
-
-            while (!valdInt)
-            {
-                try
-                {
-                    Console.WriteLine("Your Lucky number is " + intArray[intSelect]); 
-                    valdInt = true;
-                }
-                catch
-                {
-                    Console.WriteLine("sorry, that number selection is invalid. Please select a number between 0 and 9.");
-                    intSelect = Convert.ToInt32(Console.ReadLine());
-                }
-            }
-            Console.ReadLine();
-
-        }
+		}
     }
 }
