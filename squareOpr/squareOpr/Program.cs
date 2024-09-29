40b8400e2b956e8a7f8cee71c0b2f567a11a7e22
Elias Getachew <elilidu1990@gmail.com>
2024-09-29 10:04:57 -0400

    Add files via upload

---
+++ squareOpr/squareOpr/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace squareOpr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this program calculate the Area, volume and peremter of the square");
+            Console.WriteLine("Enter the side lenglth");
+            int sideLen = Convert.ToInt32(Console.ReadLine());
+            int Aresult = calAreaandPer.AreaofSquarh(sideLen);
+            Console.WriteLine("The Area of the Square is\t" + Aresult);
+            int Presult = calAreaandPer.PermeterofSquarh(sideLen);
+            Console.WriteLine("The Peremter of the Square is\t" + Presult);
+            int Vresult = calAreaandPer.VolumeofSquarh(sideLen);
+             Console.WriteLine("The Area of the Square is\t" + Vresult);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ squareOpr/squareOpr/calAreaandPer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace squareOpr
+{
+    internal class calAreaandPer
+    {
+            public static int AreaofSquarh(int len)
+            {
+                int x = len;
+                int y = x * x;
+                return y;
+            }
+            
+            public static int PermeterofSquarh(int len)
+            {
+                return 4 * len;
+            }
+            public static int VolumeofSquarh(int len)
+            {
+                return len * len*len;
+            }
+           
+        }
+    }
+
