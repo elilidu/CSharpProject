db7d1d0c761af5ebd58cb0c5573fed848de38ca9
Elias Getachew <elilidu1990@gmail.com>
2024-09-29 11:10:46 -0400

    Add Method overloading

---
+++ ArtimaticOpr/ArtimaticOpr/CalArtimatic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtimaticOpr
+{
+    public class CalArtimatic
+    {
+        public static int Artimic(int num1, int num2)
+        {
+            int x = num1;
+            int y = num2;
+            return  x+y;
+        }
+        public static int Artimic(double num1, double num2)
+        {
+            int r = Convert.ToInt32(num1 - num2);
+            return r;
+        }
+
+        public static int Artimic(string z)
+        {
+            int r = Convert.ToInt32(z);
+            return r*r;
+        }
+    }
+}
---
+++ ArtimaticOpr/ArtimaticOpr/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtimaticOpr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this program calculate the Area, volume and peremter of the square");
+            Console.WriteLine("Enter the Firest Number");
+
+            //Read the Firest number
+            int Fnum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number");
+
+            //Read the second number
+            int Snum = Convert.ToInt32(Console.ReadLine());
+
+            // call the addition method and Assigne the result
+            int Aresult = CalArtimatic.Artimic(Fnum, Snum);
+            Console.WriteLine("The Sum is\t" + Aresult);
+
+            // call the addition method and Assigne the result
+            int Sresult = CalArtimatic.Artimic(7.5, 3.2);
+            Console.WriteLine("The Diffrence is\t" + Sresult);
+
+
+            // call the addition method and Assigne the result
+            int Sqresult = CalArtimatic.Artimic("4");
+            Console.WriteLine("The Square of the number is \t" + Sresult);
+
+            Console.ReadLine();
+
+        }
+    }
+}
