ccaff4f4280dc92d6c667a251b9a560f9662d61e
Elias Getachew <elilidu1990@gmail.com>
2024-09-29 22:09:10 -0400

    Add Overloading the Equal Operator line of code

--- AbstactClassandMethod/AbstactClassandMethod/Employee.cs
+++ AbstactClassandMethod/AbstactClassandMethod/Employee.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 namespace AbstactClassandMethod
 {
     // create Employee class and inherited with superclass called person
-    public class Employee:Person, IQuittable
+    public class Employee : Person, IQuittable
     {
         public int ID { get; set; }
 
         // Implement a method in side of the Employee classes  
         public override void SayName()
         {
-            Console.WriteLine("Name\t" + FirstName+","+LastName);
+            Console.WriteLine("Name\t" + FirstName + "," + LastName);
         }
 
 
@@ -23,12 +23,38 @@ namespace AbstactClassandMethod
         {
             // The body of animalSound() is provided here
             Console.WriteLine("This line is the Impliementation of Interface Demo");
-            
+
         }
 
-       public void Quit(string myData)
+        public void Quit(string myData)
         {
-            Console.WriteLine("This line is the Impliementation of Interface and \t"+myData);
+            Console.WriteLine("This line is the Impliementation of Interface and \t" + myData);
+        }
+
+     
+         public static bool operator == (Employee emp1, Employee emp2)
+          {
+              bool status = false;
+              if (emp1.ID.Equals(emp2.ID))
+              {
+
+                  status = true;
+              }
+              return status;
+
+          }
+
+
+
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            bool status = false;
+            if (!emp1.ID.Equals(emp2.ID))
+            {
+
+                status = true;
+            }
+            return status;
         }
     }
 }
--- AbstactClassandMethod/AbstactClassandMethod/Program.cs
+++ AbstactClassandMethod/AbstactClassandMethod/Program.cs
@@ -12,10 +12,34 @@ namespace AbstactClassandMethod
         {
             // instantiate Employee Object and pass the value
             Employee employee = new Employee() {FirstName="Vinode",LastName="Zelalem" };
+
+            Employee emp1 = new Employee();
+            emp1.ID = 1;
+            emp1.FirstName = "Mekonen";
+            emp1.LastName = "Gecho";
+
+            Employee emp2 = new Employee();
+            emp2.ID = 1;
+            emp2.FirstName = "Mekonen";
+            emp2.LastName = "Gecho";
+
             // Call the SayName Method on employee object
             employee.SayName();
             employee.Quit();
             employee.Quit("Ploymorphism");
+
+            if (emp1== emp2)
+            {
+                
+                Console.WriteLine("Emp1 and empl2 are Equal");
+            }
+                
+            else
+            {
+                Console.WriteLine("Emp1 and empl2 are Not Equal");
+            }
+                
+
             Console.ReadLine();
         }
     }
