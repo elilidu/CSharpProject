f05d6e8ad1fa2b7cbb3f801824cfa906a9e45a0c
Elias Getachew <elilidu1990@gmail.com>
2024-09-30 20:18:53 -0400

    Add Struct line of code

---
+++ structDemoApp/structDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structDemo
+{
+    internal class Program
+    {
+        // Create a struct named Number
+        struct Number
+        {
+            public double Amount;
+           
+        };
+        static void Main(string[] args)
+        {
+            // Declare myNum of type Number 
+            Number myNum;
+
+            // Assign the numbet value in myNum Object
+            myNum.Amount = 78;
+
+            // Display the result
+            Console.WriteLine(" myNum Amount is :\t", myNum.Amount);
+
+            Console.ReadKey();
+
+        }
+    }
+}
