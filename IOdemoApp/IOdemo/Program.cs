b0a81c79e85db6bb8561f56540443c8b616e3769
Elias Getachew <elilidu1990@gmail.com>
2024-10-05 23:18:38 -0400

    Add IO Demo line of code

---
+++ IOdemoApp/IOdemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOdemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Enter your age ");
+            String age =Console.ReadLine();
+            File.WriteAllText("C:\\Users\\user\\source\\repos\\loge.txt",age);
+            Console.ReadLine(); 
+
+        }
+    }
+}
